3309b710474493e6cc01f7c742e6a7c55c89ead5
ArtemPC <temkaunik@gmail.com>
2024-10-11 11:51:55 +0300

    ICrudRepository

---
+++ PasswordGenerator.Database.Postgres/Repositories/Base/ICrudRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace PasswordGenerator.Database.Postgres.Repositories.Base {
+    public interface ICrudRepository<T> {
+        Task<List<T>> GetAll();
+        Task<List<T>> GetById(Guid id);
+        Task<bool> Add(T entity);
+        Task Delete(Guid id);
+        Task<bool> Update(T entity);
+    }
+}
